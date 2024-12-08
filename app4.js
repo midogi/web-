359b2112b669822592c722a2e0142c3a917ddfe2
midogi <skc04010@naver.com>
2024-12-08 16:53:22 +0900

    Create app4.js

---
+++ app4.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+res.status(200).send('<h1>Welcome !!</h1>');
+
+});
+
+app.get('/about', (req, res) => {
+res.status(200).send('<h2>Here is HJH home. </h2>');
+
+});
+app.listen(3000);
